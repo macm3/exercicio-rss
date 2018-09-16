dc12c337520027221e861a1eca56e8a8707318c7
Marcela Azevedo <macm3@cin.ufpe.br>
2018-09-16 14:56:38 -0300

    feat: passo 1

--- RSS/app/src/main/java/br/ufpe/cin/if710/rss/MainActivity.kt
+++ RSS/app/src/main/java/br/ufpe/cin/if710/rss/MainActivity.kt
@@ -2,11 +2,60 @@ package br.ufpe.cin.if710.rss
 
 import android.app.Activity
 import android.os.Bundle
+import android.widget.TextView
 
-class MainActivity : Activity() {
+import java.io.ByteArrayOutputStream
+import java.io.IOException
+import java.io.InputStream
+import java.net.HttpURLConnection
+import java.net.URL
+
+class MainActivityAntigo : Activity() {
+
+    private val RSS_FEED = "http://leopoldomt.com/if1001/g1brasil.xml"
+
+    private var conteudoRSS: TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        conteudoRSS = findViewById(R.id.conteudoRSS)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        try {
+            //Esse código dá pau, por fazer operação de rede na thread principal...
+            val feedXML = getRssFeed(RSS_FEED)
+            conteudoRSS!!.text = feedXML
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+
+    }
+
+    @Throws(IOException::class)
+    private fun getRssFeed(feed:String):String {
+        var in_: InputStream? = null
+        var rssFeed = ""
+        try{
+            val url= URL(feed)
+            val conn: HttpURLConnection = url.openConnection() as HttpURLConnection
+            in_ = conn.getInputStream()
+            val out = ByteArrayOutputStream()
+            val buffer = ByteArray(1024)
+            var count:Int = in_.read(buffer)
+            while (count != -1) {
+                out.write(buffer, 0, count)
+                count = in_.read(buffer)
+            }
+            val response = out.toByteArray()
+            rssFeed = String(response, charset("UTF-8"))
+        }finally {
+            if(in_ != null) {
+                in_.close()
+            }
+        }
+        return rssFeed
     }
 }
